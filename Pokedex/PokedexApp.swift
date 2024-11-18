f5a1de81d90777b6a41b3f054e89287ffa755bab
Karim Cordilia <k.cordilia@hotmail.com>
2024-11-18 14:45:25 +0100

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Karim Cordilia on 18/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
